c71d1ee88c561c32e2f7cd3db9ed14de09115892
nvanselow <nick@nickvanselow.com>
2017-10-26 17:58:19 -0400

    Working without turning the template string into a function

--- src/visitors/cssNamespace.js
+++ src/visitors/cssNamespace.js
@@ -5,18 +5,29 @@ import {
 } from 'babel-plugin-styled-components/lib/utils/detectors';
 
 export default (path, state) => {
-  if (isStyled(path.node.tag, state) || isHelper(path.node.tag, state)) {
-    const { tag: callee, quasi: { quasis, expressions } } = path.node;
-    const cssNamespace = state.opts.cssNamespace
-      ? `.${state.opts.cssNamespace} &`
-      : '&&';
+  const cssNamespace = state.opts.cssNamespace
+    ? `.${state.opts.cssNamespace} &`
+    : '&&';
 
-    const values = t.arrayExpression(
-      quasis.map(quasi =>
-        t.stringLiteral(`\n${cssNamespace} {${quasi.value.cooked}}\n`)
+  if (
+    (isStyled(path.node.tag, state) || isHelper(path.node.tag, state)) &&
+    path.node.quasi.quasis[0].value.cooked &&
+    !path.node.quasi.quasis[0].value.cooked.startsWith(`\n${cssNamespace} {`)
+  ) {
+    const { tag: callee, quasi: { quasis, expressions } } = path.node;
+
+    const values = quasis.map(quasi =>
+      t.templateElement(
+        {
+          cooked: `\n${cssNamespace} {${quasi.value.cooked}}\n`,
+          raw: `\n${cssNamespace} {${quasi.value.cooked}}\n`
+        },
+        quasi.tail
       )
     );
 
-    path.replaceWith(t.callExpression(callee, [values, ...expressions]));
+    path.replaceWith(
+      t.taggedTemplateExpression(callee, t.templateLiteral(values, expressions))
+    );
   }
 };
