ac552077ff04d6a9afd36e14f92f0351ef52bd8e
zhiyu <zhiyu.liu@rea-group.com>
2020-06-03 14:04:53 +0800

    Change postcss to be executed synchronously

--- src/visitors/cssNamespace.js
+++ src/visitors/cssNamespace.js
@@ -7,7 +7,6 @@ import {
   isKeyframesHelper,
   isHelper
 } from 'babel-plugin-styled-components/lib/utils/detectors';
-import { loopWhile } from 'deasync';
 import postcss from 'postcss';
 import parentSelector from 'postcss-parent-selector';
 import unnest from 'postcss-nested';
@@ -107,20 +106,17 @@ export default (path, state) => {
 
   let formattedCss = null;
   let potentialError = null;
-  postcss(processors)
-    .process(`\n${prefix} {${originalStyleString}}\n`, {
-      from: undefined // clears warning about SourceMap and Browserlist from postcss
-    })
-    .then(value => (formattedCss = value.css))
-    .catch(error => {
-      potentialError = `There was a problem adding namespaces to this CSS in the file ${
-        state.file.opts.filename
-      }. Error: ${error.message}\n CSS: ${originalStyleString}`;
-      formattedCss = 'ERROR';
-    });
-  // Allows us to turn the async promise into synchronous code for the purpose of the plugin
-  loopWhile(() => formattedCss === null);
-
+  try {
+    formattedCss = postcss(processors).process(
+      `\n${prefix} {${originalStyleString}}\n`,
+      { from: undefined }
+    ).css;
+  } catch (error) {
+    potentialError = `There was a problem adding namespaces to this CSS in the file ${
+      state.file.opts.filename
+    }. Error: ${error.message}\n CSS: ${originalStyleString}`;
+    formattedCss = 'ERROR';
+  }
   // If the css couldn't be parsed abort the plugin with an error.
   if (potentialError) {
     throw new Error(potentialError);
